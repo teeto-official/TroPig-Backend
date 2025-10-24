74cea757e4df619f2ae81d5330f6695377e15476
june <jueunlee.dev@gmail.com>
2025-10-24 20:51:30 +0900

    FIX: fix to fail test

--- src/test/kotlin/com/tropig/backend/ApplicationTests.kt
+++ src/test/kotlin/com/tropig/backend/ApplicationTests.kt
@@ -1,15 +1,8 @@
 package com.tropig.backend
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+class ApplicationTests {}
