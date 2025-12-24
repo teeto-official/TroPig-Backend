28af31b17f910da6f407668953ec08f28de74e48
june <jueunlee.dev@gmail.com>
2025-12-25 03:24:48 +0900

    feat: Mail setting 및 발송 로직 추가

---
+++ src/main/kotlin/com/tropig/backend/config/EmailConfig.kt
@@ -0,0 +1,8 @@
+package com.tropig.backend.config
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(MailProperties::class)
+class EmailConfig
