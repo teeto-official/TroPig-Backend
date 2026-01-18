01b9a7dee8f629a2ea7d6ee114358760b8dc2dfb
june <jueunlee.dev@gmail.com>
2026-01-19 00:01:01 +0900

    feat: 탈퇴하기 API 추가 modify: - 닉네임 중복 제어 추가 - 탈퇴 유저 재가입 제어 추가

--- src/main/kotlin/com/tropig/backend/member/controller/MemberController.kt
+++ src/main/kotlin/com/tropig/backend/member/controller/MemberController.kt
@@ -6,7 +6,9 @@ import com.tropig.backend.common.annotation.RequireAuth
 import com.tropig.backend.common.enums.MessageCode
 import com.tropig.backend.common.exception.NotFoundException
 import com.tropig.backend.common.model.AuthMember
+import com.tropig.backend.contents.service.ContentService
 import com.tropig.backend.member.entity.Member
+import com.tropig.backend.member.enums.Role
 import com.tropig.backend.member.model.request.SignInRequest
 import com.tropig.backend.member.model.request.SignUpRequest
 import com.tropig.backend.member.model.request.UpdateMemberRequest
@@ -14,6 +16,7 @@ import com.tropig.backend.member.model.response.MemberResponse
 import com.tropig.backend.member.model.response.TokenResponse
 import com.tropig.backend.member.service.MemberService
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import jakarta.transaction.Transactional
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -22,7 +25,8 @@ import org.springframework.web.bind.annotation.*
 @ApiController
 @RequestMapping("/api/member")
 class MemberController(
-    private val memberService: MemberService
+    private val memberService: MemberService,
+    private val contentService: ContentService,
 ) {
     @PostMapping("/sign-up")
     fun createUser(
@@ -71,5 +75,21 @@ class MemberController(
             code = MessageCode.NOT_FOUND_MEMBER
         )
         return MemberResponse.from(member)
+    }
+
+    @RequireAuth
+    @DeleteMapping
+    @Transactional
+    fun withdrawMember(
+        @AuthenticationPrincipal
+        @LoginMember authMember: AuthMember,
+    ) {
+        val deletedMember = memberService.deleteUser(authMember.memberId) ?: throw NotFoundException(
+            message = "탈퇴처리할 유저가 없습니다.",
+            code = MessageCode.NOT_FOUND_MEMBER
+        )
+        if (deletedMember.role == Role.CREATOR) {
+            contentService.updateContentToDelete(authMember.memberId)
+        }
     }
 }
