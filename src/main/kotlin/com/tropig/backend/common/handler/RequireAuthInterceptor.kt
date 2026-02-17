ca00071e907339f8cb305fd23140cacb08928b87
june <jueunlee.dev@gmail.com>
2026-02-17 23:57:45 +0900

    modify: refresh API가 호출될 수 있도록 로직 수정

--- src/main/kotlin/com/tropig/backend/common/handler/RequireAuthInterceptor.kt
+++ src/main/kotlin/com/tropig/backend/common/handler/RequireAuthInterceptor.kt
@@ -3,6 +3,7 @@ package com.tropig.backend.common.handler
 import com.tropig.backend.common.annotation.RequireAuth
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.AnonymousAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
@@ -25,7 +26,7 @@ class RequireAuthInterceptor : HandlerInterceptor {
         if (!requireAuth) return true
 
         val auth = SecurityContextHolder.getContext().authentication
-        if (auth == null || !auth.isAuthenticated) {
+        if (auth == null || !auth.isAuthenticated || auth is AnonymousAuthenticationToken) {
             response.status = HttpServletResponse.SC_UNAUTHORIZED
             return false
         }
