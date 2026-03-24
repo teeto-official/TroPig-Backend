2497f0409bad64c61583b96355651078b6ae9480
june <jueunlee.dev@gmail.com>
2026-03-24 22:23:43 +0900

    modify: SwaggerConfig 수정

--- src/main/kotlin/com/tropig/backend/config/SwaggerConfig.kt
+++ src/main/kotlin/com/tropig/backend/config/SwaggerConfig.kt
@@ -24,6 +24,8 @@ class SwaggerConfig(private val environment: Environment) {
         val activeProfiles = environment.activeProfiles
         val serverUrl = if (activeProfiles.contains("local")) {
             "http://localhost:8080"
+        } else if (activeProfiles.contains("prod")) {
+            "https://api.triquest.me"
         } else {
             "https://api-dev.triquest.me"
         }
