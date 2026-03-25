896537cd0c7a6a0fdde43b9964a9a74dc7c403c1
june <jueunlee.dev@gmail.com>
2026-03-25 23:01:05 +0900

    modify: 캐싱 제거. 추후 Redis로 변경 예정

--- src/main/kotlin/com/tropig/backend/config/CacheConfig.kt
+++ src/main/kotlin/com/tropig/backend/config/CacheConfig.kt
@@ -33,35 +33,34 @@ class CacheConfig {
 
                     // 비로그인(GUEST) 및 장르/룰 미설정 회원 랜덤 컨텐츠 캐시 (type+isAdult 기준)
                     "randomContents" -> Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
+                        .expireAfterWrite(1, TimeUnit.MINUTES)
                         .maximumSize(10)
 
                     // 장르 기반 랜덤 컨텐츠 캐시 (type+isAdult+genres 기준)
-                    "randomGenreContents" -> Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                        .maximumSize(100)
+                    // TODO: 추후 Redis로 변경. 현재는 캐싱 삭제
+//                    "randomGenreContents" -> Caffeine.newBuilder()
+//                        .expireAfterWrite(10, TimeUnit.MINUTES)
+//                        .maximumSize(100)
 
                     // 룰 기반 랜덤 컨텐츠 캐시 (isAdult+rules 기준)
-                    "randomRuleContents" -> Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES)
-                        .maximumSize(100)
+                    // TODO: 추후 Redis로 변경. 현재는 캐싱 삭제
+//                    "randomRuleContents" -> Caffeine.newBuilder()
+//                        .expireAfterWrite(10, TimeUnit.MINUTES)
+//                        .maximumSize(100)
 
                     "nicknameComponent" -> Caffeine.newBuilder()
                         .expireAfterWrite(30, TimeUnit.MINUTES) // TTL 30분 (닉네임 리스트는 자주 변경되지 않음)
 
                     // 크리에이터 발행된 작품 조회 캐시
-                    "creatorContentsByMember" -> Caffeine.newBuilder()
-                        .expireAfterWrite(30, TimeUnit.MINUTES) // TTL 30분 (발행된 작품은 자주 변경되지 않음)
-                        .maximumSize(5_000)
+                    // TODO: 추후 Redis로 변경. 현재는 캐싱 삭제
+//                    "creatorContentsByMember" -> Caffeine.newBuilder()
+//                        .expireAfterWrite(30, TimeUnit.MINUTES) // TTL 30분 (발행된 작품은 자주 변경되지 않음)
+//                        .maximumSize(5_000)
 
-                    // 크리에이터 임시저장 작품 조회 캐시
-                    "getDraftContent" -> Caffeine.newBuilder()
-                        .expireAfterWrite(5, TimeUnit.MINUTES) // TTL 5분 (임시저장은 자주 수정됨)
-                        .maximumSize(5_000)
-
-                    "revenueSummaryByMember" -> Caffeine.newBuilder()
-                        .expireAfterWrite(10, TimeUnit.MINUTES) // TTL 10분 (전체 수익/출금 금액)
-                        .maximumSize(10_000)
+                    // TODO: 추후 Redis로 변경. 현재는 캐싱 삭제
+//                    "revenueSummaryByMember" -> Caffeine.newBuilder()
+//                        .expireAfterWrite(10, TimeUnit.MINUTES) // TTL 10분 (전체 수익/출금 금액)
+//                        .maximumSize(10_000)
 
                     else -> {
                         // 디버깅: 예상하지 못한 캐시 이름이 들어오는 경우 로그 출력
