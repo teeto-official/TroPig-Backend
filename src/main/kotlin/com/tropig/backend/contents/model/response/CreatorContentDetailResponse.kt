26864a47e29259c47266c0a9cec115ae5cdaec8d
bimppap <celena1004dev@gmail.com>
2026-04-07 22:15:36 +0900

    fix: publishingInfo 필드 원복

--- src/main/kotlin/com/tropig/backend/contents/model/response/CreatorContentDetailResponse.kt
+++ src/main/kotlin/com/tropig/backend/contents/model/response/CreatorContentDetailResponse.kt
@@ -6,6 +6,8 @@ import com.tropig.backend.contents.enums.PlayerCountType
 import com.tropig.backend.contents.enums.PublishingType
 import com.tropig.backend.contents.enums.TermType
 import com.tropig.backend.contents.model.result.TagDto
+import com.tropig.backend.contents.model.serialize.PublishingInfo
+import com.tropig.backend.contents.model.serialize.toPublishingInfoList
 import java.time.LocalDateTime
 
 data class CreatorContentDetailResponse(
@@ -21,6 +23,7 @@ data class CreatorContentDetailResponse(
     val termType: TermType,
     val tags: List<TagDto>,
     val publishingType: PublishingType?,
+    val publishingInfo: List<PublishingInfo>,
     val freeContent: String?,
     val nonFreeContent: String? = null,
     val thumbnailPath: String? = null,
@@ -46,6 +49,7 @@ fun Content.toCreatorContentDetailResponse(
     termType = termType,
     tags = tags,
     publishingType = if (type == ContentType.RESOURCE) publishingType else null,
+    publishingInfo = publishingInfo?.toPublishingInfoList() ?: emptyList(),
     freeContent = freeContent,
     nonFreeContent = nonFreeContent,
     thumbnailPath = thumbnailPath,
